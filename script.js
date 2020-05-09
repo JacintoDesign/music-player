2ba6a0c43dccb3453dd88bd647a87ce1a75dd0a3
JACINTO WONG <jacinto.wong@cbc.ca>
2020-05-08 21:33:49 -0400

    final production code

--- script.js
+++ script.js
@@ -1,47 +1,43 @@
-const playerContainer = document.getElementById("player-container");
-const image = document.getElementById("image");
-const track = document.getElementById("track");
-const artist = document.getElementById("artist");
-const music = document.getElementById("music");
-const currentTimeEl = document.getElementById("current-time");
-const durationEl = document.getElementById("duration");
-const progress = document.getElementById("progress");
-const progressContainer = document.getElementById("progress-container");
-const prevBtn = document.getElementById("prev");
-const playBtn = document.getElementById("play");
-const nextBtn = document.getElementById("next");
+const image = document.getElementById('image');
+const track = document.getElementById('track');
+const artist = document.getElementById('artist');
+const music = document.getElementById('music');
+const currentTimeEl = document.getElementById('current-time');
+const durationEl = document.getElementById('duration');
+const progress = document.getElementById('progress');
+const progressContainer = document.getElementById('progress-container');
+const prevBtn = document.getElementById('prev');
+const playBtn = document.getElementById('play');
+const nextBtn = document.getElementById('next');
 
 let isPlaying = false;
 
 // Music
 const songs = [
   {
-    name: "jacinto-1",
-    displayName: "Electric Chill Machine",
-    artist: "Jacinto Design"
+    name: 'jacinto-1',
+    displayName: 'Electric Chill Machine',
+    artist: 'Jacinto Design',
   },
   {
-    name: "jacinto-2",
-    displayName: "Seven Nation Army (Remix)",
-    artist: "Jacinto Design"
+    name: 'jacinto-2',
+    displayName: 'Seven Nation Army (Remix)',
+    artist: 'Jacinto Design',
   },
   {
-    name: "jacinto-3",
-    displayName: "Goodnight, Disco Queen",
-    artist: "Jacinto Design"
+    name: 'jacinto-3',
+    displayName: 'Goodnight, Disco Queen',
+    artist: 'Jacinto Design',
   },
   {
-    name: "metric-1",
-    displayName: "Front Row (Remix)",
-    artist: "Metric/Jacinto Design"
-  }
+    name: 'metric-1',
+    displayName: 'Front Row (Remix)',
+    artist: 'Metric/Jacinto Design',
+  },
 ];
 
 // Selecting Song
 let songIndex = 0;
-
-// Load Selected Song
-loadSong(songs[songIndex]);
 
 // Update DOM
 function loadSong(song) {
@@ -51,8 +47,27 @@ function loadSong(song) {
   image.src = `img/${song.name}.jpg`;
 }
 
+// On Load - Select First Song
+loadSong(songs[songIndex]);
+
+// Play
+function playSong() {
+  isPlaying = true;
+  playBtn.classList.remove('fa-play');
+  playBtn.classList.add('fa-pause');
+  music.play();
+}
+
+// Pause
+function pauseSong() {
+  isPlaying = false;
+  playBtn.classList.remove('fa-pause');
+  playBtn.classList.add('fa-play');
+  music.pause();
+}
+
 // Play or Pause Event Listener
-playBtn.addEventListener("click", () => {
+playBtn.addEventListener('click', () => {
   if (isPlaying) {
     pauseSong();
   } else {
@@ -60,86 +75,68 @@ playBtn.addEventListener("click", () => {
   }
 });
 
-// Play
-function playSong() {
-  isPlaying = true;
-  playBtn.innerHTML = `<i class="fas fa-pause main-button" title="Pause"></i>`;
-  music.play();
-}
-
-// Pause
-function pauseSong() {
-  isPlaying = false;
-  playBtn.innerHTML = `<i class="fas fa-play main-button" title="Play"></i>`;
-  music.pause();
-}
-
 // Set Progress Bar
-progressContainer.addEventListener("click", setProgressBar);
 function setProgressBar(e) {
-  let width = this.clientWidth;
+  const width = this.clientWidth;
   // shows total width
   // console.log(width);
-  let clickX = e.offsetX;
-  let duration = music.duration;
+  const clickX = e.offsetX;
+  const { duration } = music;
   music.currentTime = (clickX / width) * duration;
 }
 
 // Update Progress Bar & Time
-music.addEventListener("timeupdate", updateProgressBar);
 function updateProgressBar(e) {
   if (isPlaying) {
-    let { duration, currentTime } = e.srcElement;
-    //console.log(duration, currentTime);
+    const { duration, currentTime } = e.srcElement;
+    // console.log(duration, currentTime);
 
     // Calculate display for duration
-    let durationMinutes = Math.floor(duration / 60);
+    const durationMinutes = Math.floor(duration / 60);
     let durationSeconds = Math.floor(duration % 60);
     if (durationSeconds < 10) {
-      durationSeconds = "0" + durationSeconds;
+      durationSeconds = `0${durationSeconds}`;
     }
     // Delay switching duration Element to avoid NaN
     if (durationSeconds) {
       durationEl.innerText = `${durationMinutes}:${durationSeconds}`;
     }
     // Calculate display for currentTime
-    let currentMinutes = Math.floor(currentTime / 60);
+    const currentMinutes = Math.floor(currentTime / 60);
     let currentSeconds = Math.floor(currentTime % 60);
     if (currentSeconds < 10) {
-      currentSeconds = "0" + currentSeconds;
+      currentSeconds = `0${currentSeconds}`;
     }
     currentTimeEl.innerText = `${currentMinutes}:${currentSeconds}`;
     // Update progress bar width
-    let progressPercent = (currentTime / duration) * 100;
+    const progressPercent = (currentTime / duration) * 100;
     progress.style.width = `${progressPercent}%`;
   }
 }
 
 // Previous Song
-prevBtn.addEventListener("click", prevSong);
 function prevSong() {
   songIndex--;
-
   if (songIndex < 0) {
     songIndex = songs.length - 1;
   }
-
   loadSong(songs[songIndex]);
   playSong();
 }
 
 // Next Song
-nextBtn.addEventListener("click", nextSong);
 function nextSong() {
   songIndex++;
-
   if (songIndex > songs.length - 1) {
     songIndex = 0;
   }
-
   loadSong(songs[songIndex]);
   playSong();
 }
 
-// Play next song when current finishes
-music.addEventListener("ended", nextSong);
+// Event Listeners
+prevBtn.addEventListener('click', prevSong);
+nextBtn.addEventListener('click', nextSong);
+music.addEventListener('ended', nextSong);
+progressContainer.addEventListener('click', setProgressBar);
+music.addEventListener('timeupdate', updateProgressBar);
